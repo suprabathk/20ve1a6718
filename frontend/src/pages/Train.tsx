2ba2832fee59b47a66462678dd4ea3e1cf932d21
suprabathk <suprabathkondapally@gmail.com>
2023-08-07 14:23:25 +0530

    Added fields to Train page

--- frontend/src/pages/Train.tsx
+++ frontend/src/pages/Train.tsx
@@ -33,7 +33,22 @@ export default function Trains() {
                         <p><span className="text-xl font-semibold">Departure time: </span>{`${train.departureTime.Hours}:${train.departureTime.Minutes}:${train.departureTime.Seconds}`}</p>
                         <p><span className="text-xl font-semibold">Delayed by: </span>{train.delayedBy}</p>
                     </div>
-                    <div></div>
+                    <div className="flex items-center mt-10 justify-between ">
+                        <div >
+                            <p className="font-semibold text-2xl">Seats Available:</p>
+                            <div className="flex flex-col ml-4">
+                                <p><span className="font-semibold text-xl">Sleeper:</span> {train.seatsAvailable.sleeper} seats</p>
+                                <p><span className="font-semibold text-xl">AC:</span> {train.seatsAvailable.AC} seats</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="font-semibold text-2xl">Price:</p>
+                            <div className="flex flex-col ml-4">
+                                <p><span className="font-semibold text-xl">Sleeper:</span> {train.price.sleeper} rs</p>
+                                <p><span className="font-semibold text-xl">AC:</span> {train.price.AC} rs</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>}
         </div>
     )
